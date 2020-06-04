14851221454a63dc28823e63539e945cd8d07c8b
xogud02 <xogud02@naver.com>
2020-06-04 17:27:06 +0900

    manage direction logics by vector

--- Classes/Character.cpp
+++ Classes/Character.cpp
@@ -4,7 +4,7 @@
 USING_NS_CC;
 using namespace std;
 
-Character* Character::createCharacter(float unitSize, int scale) {
+Character* Character::create(float unitSize, int scale) {
 	Character* ret = new Character(scale);
 	if (!ret || !ret->initWithFile("Characters/Warrior.png", CC_RECT_PIXELS_TO_POINTS(Rect(0, 0, 16, 16)))) {
 		CC_SAFE_DELETE(ret);
@@ -12,10 +12,10 @@ Character* Character::createCharacter(float unitSize, int scale) {
 	}
 
 	string img = "Characters/Warrior.png";
-	ret->moveDown = createMoveAction(img, 0);
-	ret->moveLeft = createMoveAction(img, 1);
-	ret->moveRight = createMoveAction(img, 2);
-	ret->moveUp = createMoveAction(img, 3);
+	ret->directions.push_back(make_pair([](float angle) {return -135 < angle && angle <= -45; }, createMoveAction(img, 0)));//downWard
+	ret->directions.push_back(make_pair([](float angle) {return 135 < angle || angle <= -135; }, createMoveAction(img, 1)));//leftWard
+	ret->directions.push_back(make_pair([](float angle) {return -45 < angle && angle <= 45; }, createMoveAction(img, 2)));//rightWard
+	ret->directions.push_back(make_pair([](float angle) {return 45 < angle && angle <= 135; }, createMoveAction(img, 3)));//upWard
 
 	ret->autorelease();
 	float characterSize = unitSize * ret->SCALE;
@@ -58,7 +58,14 @@ Character::Character(int scale) :SCALE(scale)
 {
 }
 
-void Character::tick(float dt) {
+Character::~Character()
+{
+	for (auto direction : directions) {
+		direction.second->release();
+	}
+}
+
+void Character::move(float dt) {
 	auto currentPos = getPosition();
 	auto direction = (nextPos - currentPos);
 	auto normalized = direction.getNormalized();
@@ -66,37 +73,29 @@ void Character::tick(float dt) {
 	float moveDist = speed * dt;
 
 	if (direction.length() < moveDist) {
-		unschedule(CC_SCHEDULE_SELECTOR(Character::tick));
-		CCLOG("arrive at (%f, %f)", currentPos.x, currentPos.y);
+		unschedule(CC_SCHEDULE_SELECTOR(Character::move));
+		stopAction(currentAction);
+		currentAction = nullptr;
 		return;
 	}
 	setPosition(currentPos + normalized * moveDist);
 }
 
-void Character::move(Vec2 position)
+void Character::moveTo(Vec2 position)
 {
 	Vec2 direction = position - getPosition();
-	float angle = CC_RADIANS_TO_DEGREES(direction.getAngle());
 	if (currentAction != nullptr) {
 		stopAction(currentAction);
 	}
-	CCLOG("angle : %f", angle);
-	if (-45 < angle && angle <= 45) {
-		currentAction = runAction(moveRight);
-	}
-	else if (45 < angle && angle <= 135) {
-		currentAction = runAction(moveUp);
-	}
-	else if (-135< angle && angle <= -45) {
-		currentAction = runAction(moveDown);
-	}
-	else {
-		currentAction = runAction(moveLeft);
+
+	float angle = CC_RADIANS_TO_DEGREES(direction.getAngle());
+	for (auto direction : directions) {
+		if (direction.first(angle)) {
+			currentAction = runAction(direction.second);
+			break;
+		}
 	}
 
 	nextPos = position;
-	if (this->isScheduled(CC_SCHEDULE_SELECTOR(Character::tick))) {
-		CCLOG("moving!!");
-	}
-	this->schedule(CC_SCHEDULE_SELECTOR(Character::tick));
+	this->schedule(CC_SCHEDULE_SELECTOR(Character::move));
 }
