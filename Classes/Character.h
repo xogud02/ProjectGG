2dc5b5cd664b9b32eaf0404fe0e839815e91bb65
xogud02 <xogud02@naver.com>
2020-06-06 23:59:48 +0900

    moved function

--- Classes/Character.h
+++ Classes/Character.h
@@ -3,18 +3,22 @@
 #include "cocos2d.h"
 #include <vector>
 #include <functional>
+#include "Grid.h"
+
+using GridPosition = std::pair<int, int>;
 
 class Character : public cocos2d::Sprite {
 	int speed = 100;
 	cocos2d::Vec2 nextPos;
-
 	cocos2d::Action* currentAction;
 
 	std::vector<std::pair<std::function<bool(float)>, cocos2d::Action*>> directions;
+	GridPosition currentGridPosition;
 
 	void move(float);
 public:
 	void moveTo(cocos2d::Vec2);
+	void tryToMove(cocos2d::Vec2);
 
 	const int SCALE;
 
--- Classes/Grid.h
+++ Classes/Grid.h
@@ -9,16 +9,15 @@ using GridPosition = std::pair<int, int>;
 
 class Grid :public cocos2d::LayerColor{
 	std::vector<std::vector<bool>> movableGrid;
-	const float UNIT_SIZE;
 	Character* player;
-
-	bool isMovable(int row, int col);
 
 	void setPlayer(Character* character);
 
+public:
+	const float UNIT_SIZE;
+	bool isMovable(int row, int col);
 	GridPosition vecToGrid(cocos2d::Vec2 position);
 	cocos2d::Vec2 gridToPosition(GridPosition gridPosition);
-public:
 	int getRows();
 	int getCols();
 	static Grid* createGrid(int rows, int cols);
