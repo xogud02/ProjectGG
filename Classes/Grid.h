e67c388c119d645cdf656cef2ead225178bc7160
wlsvy <hubjpkim@gmail.com>
2020-06-09 23:34:03 +0900

    test merge

--- Classes/Grid.h
+++ Classes/Grid.h
@@ -2,10 +2,12 @@
 
 #include "cocos2d.h"
 #include "cocos-ext.h"
-#include "Character.h"
 #include <vector>
+#include <array>
 
-using GridPosition = std::pair<int, int>;
+
+using GridPosition = std::pair<int, int>;//TODO ¡Æ©ø¨ù¡¾
+using UINT = unsigned int;
 
 enum class TileTypes : std::uint8_t {
 	Default = 0,
@@ -37,21 +39,32 @@ private:
 	TileTypes m_Type;
 };
 
-class Grid :public cocos2d::LayerColor{
-	std::vector<std::vector<bool>> movableGrid;
-	Character* player;
-
-	void setPlayer(Character* character);
-
+class Character;
+class Grid : public cocos2d::LayerColor {
 public:
 	const float UNIT_SIZE;
+
 	bool isMovable(int row, int col);
 	GridPosition vecToGrid(cocos2d::Vec2 position);
 	cocos2d::Vec2 gridToPosition(GridPosition gridPosition);
 	int getRows();
 	int getCols();
-	static Grid* createGrid(int rows, int cols);
 	Grid(int rows, int cols, float unitSize);
 	void showGrid();
 	bool onTouch(cocos2d::Touch* t, cocos2d::Event* e);
+	cocos2d::Vec2 getPositionOffset();
+	void occupyArea(const GridPosition, const int size = 1);
+
+	static Grid* createGrid(int rows, int cols);
+
+private:
+	std::vector<std::vector<bool>> movableGrid;
+
+	const UINT row;
+	const UINT coloum;
+	Character* player;
+
+	cocos2d::DrawNode* debugGrid;
+
+	void setPlayer(Character* character);
 };
