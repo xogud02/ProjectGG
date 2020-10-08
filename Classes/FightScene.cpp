09272a2520e7f777a98bea2dbb76ea5baed39f15
xogud02 <xogud02@naver.com>
2020-10-08 14:27:38 +0900

    remove useless code

--- Classes/FightScene.cpp
+++ Classes/FightScene.cpp
@@ -33,14 +33,7 @@
 #include "GUIBoxCreator.h"
 
 USING_NS_CC;
-USING_NS_CC_EXT;
 using namespace std;
-
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename) {
-	printf("Error while loading: %s\n", filename);
-	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
 
 // on "init" you need to initialize your instance
 bool FightScene::init() {
