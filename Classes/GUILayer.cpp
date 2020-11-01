bf152bfad196afba82e517a66b243b1c248f64f0
xogud02 <xogud02@naver.com>
2020-11-01 22:21:50 +0900

    test cooldown icon

--- Classes/GUILayer.cpp
+++ Classes/GUILayer.cpp
@@ -4,6 +4,8 @@
 #include "Player.h"
 #include "CharacterSpriteFactory.h"
 #include "MonsterSpriteFactory.h"
+#include "SpriteFactory.h"
+#include "TTFLabelBuilder.h"
 
 USING_NS_CC;
 using namespace std;
@@ -37,8 +39,8 @@ bool GUILayer::init() {
 
 	instance = this;
 
-	schedule([this](float) mutable{
-		
+	schedule([this](float) mutable {
+
 		removeDied(characterBarMap);
 		canvas->clear();
 		for (const auto& p : characterBarMap) {
@@ -127,7 +129,7 @@ private:
 		auto thumbNail = Sprite::createWithSpriteFrame(CharacterSpriteFactory::characterFrame(character));
 		thumbNail->setPosition(Vec2::ONE*leftSize / 2);
 		auto targetSize = leftSize - boxCreator.edgeThickness * 2;
-		thumbNail->setScale(targetSize / thumbNail->getContentSize().width);
+		thumbNail->setScale(SpriteFactory::getUnitScale(targetSize));
 		return thumbNail;
 	}
 
@@ -135,13 +137,82 @@ private:
 		boxCreator.size = Size(size.width - leftSize, size.height);
 		auto right = boxCreator.create();
 		right->setPosition(Vec2(leftSize, 0));
-		boxCreator.size = Size(size.height / 2, size.height / 2);
+
+		auto iconSize = size.height / 2;
+		boxCreator.size = Size(iconSize, iconSize);
 		auto icon = boxCreator.create();
 		auto blink = Sprite::create();
 		blink->runAction(MonsterSpriteFactory::createMonsterAnimation(ElementalType::LightBlink));
+		blink->setPosition(Vec2::ONE * iconSize / 2);
+		auto innerIconSize = iconSize - boxCreator.edgeThickness * 2;
+		blink->setScale(SpriteFactory::getUnitScale(innerIconSize));
 		icon->addChild(blink, 1);
-		right->addChild(icon);
 
+		auto txt = TTFLabelBuilder().setTextSize(30).build("3");
+		icon->addChild(txt, 3);
+		txt->setPosition(Vec2::ONE * iconSize / 2);
+
+		auto draw = DrawNode::create();
+
+		float cooldown = 10.f;
+		
+		draw->schedule([draw, time = cooldown, remain = cooldown, size = iconSize, txt](float delta)mutable {
+			draw->clear();
+			auto degree = 360 - 360 * remain / time;
+			auto calcDegree = 450 - degree;
+			while (calcDegree > 360) {
+				calcDegree -= 360;
+			}
+			txt->setString(to_string((int)remain));
+			auto center = size / 2;
+			//y - c = tan(d)(x-c)
+			//y = tan(d)(x-c) + c
+			//x = (y-c)/tan(d) + c
+
+			vector<Vec2> points;
+			
+			auto color = Color4F(0, 0, 1, 0.5f);
+			Vec2 c(center, center), t(center, size), tl(0, size), tr(size, size), bl(0, 0), br(size, 0), b(center, 0);
+			auto tan = ::tan(CC_DEGREES_TO_RADIANS(calcDegree));
+			if (45 < calcDegree && calcDegree < 90) {//topright
+				draw->drawSolidRect(bl, t, color);
+				points = { c, Vec2(center / tan + center,size) ,tr,br,b };
+			} else if (calcDegree == 90) {
+				points = { tl,tr,br,bl };
+			} else if (90 < calcDegree && calcDegree <= 135) {//topleft
+				points = { Vec2(center / tan + center,size) ,t,c };
+			} else if (135 < calcDegree && calcDegree <= 225) {//left
+				Vec2 v(0, center);
+				if (calcDegree != 180) {
+					v.y = -tan * center + center;
+				}
+				points = { tl,t,c,v };
+			} else if (225 < calcDegree && calcDegree <= 315) {//bottom
+				Vec2 v(center, 0);
+				if (calcDegree != 270) {
+					v.x = -center / tan + center;
+				}
+				points = { tl, t,c,v,bl };
+			} else {//right
+				draw->drawSolidRect(bl, t, color);
+				Vec2 v(size, center);
+				if (calcDegree != 0) {
+					v.y = tan * center + center;
+				}
+				points = { c,v,br,b };
+			}
+
+			draw->drawPolygon(points.data(), points.size(), Color4F(0, 0, 1, 0.5f), 0, Color4F::BLACK);
+
+			remain -= delta;
+			if (remain < 0) {
+				remain += time;
+			}
+		}, "draw");
+
+		icon->addChild(draw, 2);
+
+		right->addChild(icon);
 		return right;
 	}
 };
