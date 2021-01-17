9a053c5c48da802c435bcfb1079f8138b3b76780
xogud02 <xogud02@gmail.com>
2021-01-17 23:27:04 +0900

    more fields

--- Classes/Door.h
+++ Classes/Door.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "cocos2d.h"
 #include "GridObject.h"
 #include "SpriteTileEnums.h"
 
 class Door : public GridObject {
-	DoorLockType doorLockType;
-	DoorPosition doorPosition;
-	DoorType doorType;
+	DoorLockType doorLockType = DoorLockType::None;
+	DoorPosition doorPosition = DoorPosition::Horizontal;
+	DoorType doorType = DoorType::Wood;
+	cocos2d::Sprite* opened = nullptr;
+	cocos2d::Sprite* closed = nullptr;
 };
