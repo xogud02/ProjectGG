fe772a0be1d95fe9be2592a9dbd5f6dd68eb20fd
xogud02 <xogud02@gmail.com>
2021-01-21 23:09:38 +0900

    trigger type

--- Classes/GridObject.h
+++ Classes/GridObject.h
@@ -7,13 +7,14 @@
 
 using TileMap = std::unordered_map<GridPosition, cocos2d::Sprite*>;
 using TileTypeMap = std::unordered_map<GridPosition, TileType>;
+class Character;
 
 class GridObject :public cocos2d::Node{
 	TileMap tiles;
 	TileTypeMap tileTypes;
 public:
-	std::function<void()> onTriggerIn;
-	std::function<void()> onTriggerOut;
+	std::function<void(Character*)> onTriggerIn;
+	std::function<void(Character*)> onTriggerOut;
 
 	CREATE_FUNC(GridObject);
 	const TileMap& getTiles() const;
